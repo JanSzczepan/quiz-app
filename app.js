9a1e5831e521d0d38ff18207fe8faca3f312b3c7
Jan Szczepan <janszczepan62@gmail.com>
2022-05-02 19:15:20 +0200

    Port fixed

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ import gameRoutes from './routes/game.js';
 const __dirname = path.resolve();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-   console.log(`Server is listening at http://localhost:3000/ Let's play the game :)`);
+   console.log(`Server is listening at http://localhost:${port}/ Let's play the game :)`);
 });
 
 //wskazujemy miejsce, z którego chcemy dostarczać statyczne pliki (index.html itp.)
